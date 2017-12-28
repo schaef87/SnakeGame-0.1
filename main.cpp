d10f587ac7f5ee639ebb11da12b1a68066adb8db
Justin Schaefer <jds_06@hotmail.com>
2017-12-28 13:20:45 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,87 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 28, 2017
+ *      Author: Justin Schaefer
+ */
+
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 28, 2017
+ *      Author: Justin Schaefer
+ *      Using the tutorial found here: https://youtu.be/E_-lMZDi7Uw
+ */
+
+#include <iostream>
+using namespace std;
+
+//Variables
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x,y, fruitX, fruitY, score;
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
+void Setup()
+{
+	gameOver = false;
+	dir = STOP;
+	x = width/2;
+	y = height/2;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score = 0;
+}
+
+void Draw()
+{
+	system("cls");
+	for (int i = 0; i < width; i++)
+		cout << "#";
+	cout << endl;
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if (j == 0)
+			cout << "#";
+
+			cout << " ";
+
+			if (j == width-1)
+			cout << "#";
+		}
+		cout << endl;
+	}
+
+	for (int i =0; i < width; i++)
+		cout << "#";
+	cout << endl;
+
+}
+
+void Input()
+{
+
+}
+
+void Logic()
+{
+
+}
+
+int main()
+{
+	Setup();
+	while (!gameOver)
+	{
+		Draw();
+		Input();
+		Logic();
+		//Sleep(1);
+	}
+	return 0;
+}
